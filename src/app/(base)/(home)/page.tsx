887dfd5310f37f1939bbc5c5f8a05c3149260183
Adriel Zimbril <109924237+adrielzimbril@users.noreply.github.com>
2025-09-06 13:53:34 +0000

    feat: Remove QuoteSection and obsolete TestimonialsSection-simple component

--- src/app/(base)/(home)/page.tsx
+++ src/app/(base)/(home)/page.tsx
@@ -3,8 +3,7 @@ import { CallToAction } from "@/components/shared/pages/shared/call-to-action";
 import { HeaderSection } from "./sections/HeaderSection";
 import { ProjectsSection } from "./sections/ProjectsSection";
 import { ResourcesSection } from "./sections/ResourcesSection";
-import { QuoteSection } from "./sections/QuoteSection";
-import { TestimonialsSection } from "./sections/TestimonialsSection-simple";
+import { TestimonialsSection } from "./sections/TestimonialsSection";
 
 export default function Home() {
   return (
